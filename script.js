ff36ad77cb317bb4365d81a788eae81a61cd66c6
FabeehaAhmed <49812761+FabeehaAhmed@users.noreply.github.com>
2019-07-16 14:03:15 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,317 @@
+let techKaro = {
+
+    courseName: "Tech Karo",
+    students: [
+        {
+            name: "shabana",
+            fatherName: "Ahmed",
+            fatherProfession: "Teaching",
+            hobby: "Gardening",
+            contactNo: " 0321464858",
+            dateOfBirth: 20 / 8 / 1998,
+            gender: "Female",
+            group: "A",
+            emailAddress: "fabeehaahmed01@outlook.com",
+            qualification: "Inter",
+            sibblings: "2",
+            cnic: 42101 - 2378375389 - 9,
+            ambassadorName: "Arsala",
+        },
+        {
+            name: "Pervaiz",
+            fatherName: "Fareed",
+            fatherProfession: "cashier",
+            hobby: "LearningBooks",
+            contactNo: "0347808501",
+            dateOfBirth: "06/07/1996",
+            gender: "Male",
+            group: "B",
+            emailAddress: "pervaizshah083@gmail.com",
+            qualification: "softwareEngineer",
+            sibblings: "",
+            cnic: "42401-677853576-878",
+            ambassadorName: "Noreen Naz",
+        },
+        {
+            name: "Erum",
+            fatherName: "Razakhan",
+            fatherProfession: "civil engineer",
+            hobby: "study",
+            contactNo: "03546783",
+            dateOfBirth: "09-09-2001",
+            gender: "Female",
+            group: "B",
+            emailAddress: "erum765@SpeechGrammarList.com",
+            qualification: "electricalEngineer",
+            sibblings: "8",
+            cnic: "4567-989877889",
+            ambassadorName: "sir faizan",
+        },
+        {
+            name: "Tehreem",
+            fatherName: "Ali",
+            fatherProfession: " Professor in uni",
+            hobby: "listening song",
+            contactNo: "0303876589",
+            dateOfBirth: "06-01-1996",
+            gender: "female",
+            group: "c",
+            emailAddress: "teh867@outlook.com",
+            qualification: "MBBS",
+            sibblings: "5",
+            cnic: "424-89765-00",
+            ambassadorName: "MAM-Noreen",
+        },
+        {
+
+            name: "rehan khan",
+            fatherName: "Asghar khan",
+            fatherProfession: "Mechanical engineer",
+            hobby: "horseRiding",
+            contactNo: "0321-87766",
+            dateOfBirth: "14-09-1997",
+            gender: "male",
+            group: "A",
+            emailAddress: "Rehan987@tootlook.com",
+            qualification: "ambassador in techkaro",
+            sibblings: "4",
+            cnic: "4323-756898",
+            ambassadorName: "MAMArsala",
+        },
+        {
+            name: " Md-azeem",
+            fatherName: "Md-naeem",
+            fatherProfession: "policeman",
+            hobby: "driving",
+            contactNo: "0345-87609",
+            dateOfBirth: "31-09-2019",
+            gender: "male",
+            group: "B",
+            emailAddress: "md654@web.com",
+            qualification: "masters",
+            sibblings: "6",
+            cnic: "424-0965784",
+            ambassadorName: "Ali",
+        },
+        {
+            name: "mehreenAkhter",
+            fatherName: "farhanAkhter",
+            fatherProfession: "clerkIn-office",
+            hobby: "running",
+            contactNo: "021-9877689",
+            dateOfBirth: "25-06-1995",
+            gender: "female",
+            group: "A",
+            emailAddress: "Akhter432@goodlook.com",
+            qualification: "softEngineer",
+            sibblings: "5",
+            cnic: "4562-87155",
+            ambassadorName: "mam Noreen",
+        },
+        {
+            name: "Reenaroy",
+            fatherName: "roy",
+            fatherProfession: "diplomaEngineer",
+            hobby: "singing",
+            contactNo: "0345-987687",
+            dateOfBirth: "07-05-2011",
+            gender: "male",
+            group: "B",
+            emailAddress: "Reena563@reeno.com",
+            qualification: "biologicalEngineer",
+            sibblings: "4",
+            cnic: "4241-897509",
+            ambassadorName: "sirAli",
+        },
+
+        {
+            name: "kokab",
+            fatherName: "ishtiaq",
+            fatherProfession: "managerInBank",
+            hobby: "playGame",
+            contactNo: "0347-646860",
+            dateOfBirth: "03-02-1998",
+            gender: "male",
+            group: "B",
+            emailAddress: "kokab667@gmail.com",
+            qualification: "gratuation",
+            sibblings: "1",
+            cnic: "42401-876578",
+            ambassadorName: "MAM-Arsala",
+        },
+        {
+            name: "hania Khan",
+            fatherName: "Asghar khan",
+            fatherProfession: "mechanical Engineer",
+            hobby: "recitingHolyQuran",
+            contactNo: "0321-508766",
+            dateOfBirth: "07-03-1999",
+            gender: "Female",
+            group: "A",
+            emailAddress: "Khan765@june.com",
+            qualification: "Doctor",
+            sibblings: "",
+            cnic: "4312-76094",
+            ambassadorName: "MAM-Noreen",
+        },
+    ],
+
+    batchNo:
+
+        "This is the batch of 2019",
+
+    ambassadors: [
+
+        {
+            name: "Arsala",
+            cnic: "4240-732877",
+            qualification: "WebDeveloper",
+            profilePic: "",
+            email: "arsala756@arsal.com",
+            Income: "30,000",
+            personalIncome: "40,000",
+            sibilings: "4",
+            isEmployed: "true",
+            reference: "she is brillient student here",
+            extraSkills: "veryHardworking",
+        },
+        {
+            name: "Noreen Naz",
+            cnic: "4240-732877",
+            qualification: "WebDeveloper",
+            profilePic: "",
+            email: "noreen856@noor.com",
+            Income: "30,000",
+            personalIncome: "50,000",
+            sibilings: "3",
+            isEmployed: "true",
+            reference: "she is brillient student here",
+            extraSkills: "neverTired",
+        },
+        {
+            name: "Ali",
+            cnic: "4240-732877",
+            qualification: "WebDeveloper",
+            profilePic: "",
+            email: "ali756@ali.com",
+            Income: "30,000",
+            personalIncome: "60,000",
+            sibilings: "4",
+            isEmployed: "true",
+            reference: "he is brillient student here",
+            extraSkills: "work all the time",
+        },
+    ],
+    duration:
+        "In tech karo the course duration is eight months",
+    sponsor:
+        "Tech karo is sponsored by engroVopak",
+    timings:
+        "2pm to 4pm and practice hour 4pm to 6pm",
+    fees:
+        "The fee is 500 rupee per syudent only",
+    expenses:
+        "Tech karo expense money per month to the students 40 lacks",
+
+    classes:
+        "one class for group A and one class for group B",
+    location:
+        "ST-13.Block-7,Gulshan-e-Iqbal,Abul Hasan Isphahani Road.Karachi-75300",
+    techkaroTelNo:
+        "34978274-75,34982464-76",
+    lifeSkills:
+        "Every Saturday life skill class is held",
+    syllabus:
+        "The tech karo syllabus is html,css and javascript,wordPress and react",
+    purpose:
+        "To teach students how to thinking by coding and to be make a webDeveloper to them",
+    exams:
+        "every at the end of the month student's test is held",
+    project: [
+        {
+            name1: "slack project",
+            name2: "fruitshop project",
+            name3: "techkaro project",
+            name4: "array of jason",
+
+        },
+    ],
+    successStory: [{
+        name: "Noreen Naz",
+        sibilings: "she has three brother and three sisters",
+        discription: "Before she came in Tech karo she was an incharge in her school although she heard about tech karo from her school, after she came in tech karo",
+    },
+    ],
+    events: [
+        {
+            date: "january-07-2019",
+            techKaro: "To close the digital gender divide , tech karo was initiated in karachi on 22nd february 2018. Tech karo is an 8 months demand driven training program run by CIRCLE, Where students are provided with tech education to enhance their economic oppertunities , tech mentors to provide inspiration to change mindsets , and special training for women to overcome their constraints and embrace the future as tech professionals.Students are taught front-end web development through software like HTML, CSS and javascript programing and mobile web apps.Tech karo has been a huge success and has received an overwhelming response from the students , most of whom are determined to join the work force and are eagerly looking forward to making their presence in the field of tech",
+            otherEvent: "Tech karo also celebrates Eid party every year",
+            karachi:"Circle a social enterprise focused on women's economic empowerment, held a launch ceremony yesterday for the new phase of its highly successful Tech karo program which aims to create a pipeline of youth in tech by providing web development and life skills, with a special focus on women and those from underserved communities",
+
+        },
+    ],
+
+
+
+    studentsExperience: [
+        {
+            name: "Rabia aziz",
+            experience: "i am very grateful to Tech karo.Prior to joining the program,i used to work as a teacher, working long hours and earning a low salary.Now, i am a front end web developer in term at ibex, learning new things and earning much more",
+        },
+
+        {
+            name: "Maria azhar",
+            experience: "I discovered a new love for coding when i joined Tech karo. After 8 months of training,i have been offered an intership at IBEX.I want to thank Tech karo for giving me a new life",
+        },
+
+        {
+            name: "Shagufta Islam",
+            experience: "Before Tech karo, i did not know about any type of computer languages, how to use them and what is the result of each.Thanks to Tech karo,Now i am able to use these languages",
+        },
+
+        {
+            name: "Niha Amin",
+            experience: "Before joining Tech karo i did not know the technicality of web designing.However, after joining IBEX as an intern, i have gained UI/UX designing experience and have been appreciated by my teamlead  for brilliant animations",
+        }
+    ],
+
+
+    Teachers: [
+        {
+            name: "Sarah Ahmed",
+            email: "sarah456@sarah.com",
+            cnic: "42401-56876",
+            wentTo: "NED University",
+            live: "In Karachi",
+            qualification: "Software Engineer",
+
+        },
+
+        {
+            name: "Tooba Mohsin khan",
+            email: "toobamohsin876@gmail.com",
+            cnic: "42401-645787",
+            wentTo: "NED University",
+            live: "In karachi",
+            qualification: "Software Engineer",
+
+        },
+    ],
+    rooms: [
+        {
+            noOfRooms: "There are 900 rooms in tech karo",
+            washRoom: "45",
+            gcr: "4",
+            audotorium: "6",
+        },
+    ],
+    
+
+
+},
+
+
+
+
+
